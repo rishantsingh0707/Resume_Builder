288ff7f3bd57d87866f59b1ca65e148851edaabf
Rishant <rishantsingh0707@gmail.com>
2026-06-17 23:00:14 +0530

    changed cors funvtiom

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,5 @@ import axios from "axios";
 
 export default axios.create({
     baseURL:
-        "http://localhost:5000/api" || 
-         import.meta.env.VITE_API_URL, 
+               import.meta.env.VITE_API_URL, 
 });
